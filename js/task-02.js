205c6c3f6d4769f53940897289774db34a23ce1d
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-01-15 23:50:19 +0100

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+
+  const itemEl = document.createElement('li');
+  console.log(itemEl);
+
+  itemEl.textContent = ingredient;
+
+  itemEl.className = 'item';
+
+  listEl.append(itemEl);
+}
+
+console.log(listEl);
